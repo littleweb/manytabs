d1639c36f1ebd60a26141fc880ea32437ea9c088
libin <girlblank@gmail.com>
2012-07-30 23:41:10 +0800

    发布第一个版本。
    分为两个核心文件：.hta，.bat；
    .bat初始化项目目录，.hta提供针对各目录的文件浏览；

---
+++ 项目目录初始化.bat
@@ -0,0 +1,29 @@
+mkdir 产品资料
+cd 产品资料
+mkdir MRD
+mkdir 原型
+cd ..
+
+mkdir UI设计图
+cd UI设计图
+mkdir 生成图
+mkdir PSD源图
+cd ..
+
+mkdir 工程设计资料
+cd 工程设计资料
+mkdir 设计文档
+mkdir 碎片资料
+cd ..
+
+mkdir 编码开发
+cd 编码开发
+mkdir 开发
+mkdir 源码.bak
+cd ..
+
+mkdir 测试
+
+mkdir 数据
+
+mkdir DEMO
